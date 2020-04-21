5c76e5897919740dd030f8a5486a08939e597cc5
Julia0212 <yulia.samartseva@gmail.com>
2020-04-21 16:57:52 +0300

    Add Strategy design pattern

--- SortingVisualiser/sortingalgorithms.cpp
+++ SortingVisualiser/sortingalgorithms.cpp
@@ -1,6 +1,2 @@
 #include "sortingalgorithms.h"
 
-SortingAlgorithms::SortingAlgorithms()
-{
-
-}
--- SortingVisualiser/sortingalgorithms.h
+++ SortingVisualiser/sortingalgorithms.h
@@ -2,12 +2,52 @@
 #define SORTINGALGORITHMS_H
 #include <vector>
 
-
-class SortingAlgorithms
+class SortingStrategy
 {
 public:
-    SortingAlgorithms();
-    virtual void sort(vector<int>& list) = 0;
+    SortingStrategy();
+    virtual void sort(std::vector<int> list) = 0;
 };
+
+class QuickSort : public SortingStrategy {
+    void sort(std::vector<int> list) override;
+};
+
+class MergeSort : public SortingStrategy {
+    void sort(std::vector<int> list) override;
+};
+
+class InsertionSort : public SortingStrategy {
+    void sort(std::vector<int> list) override;
+};
+
+class SelectionSort : public SortingStrategy {
+    void sort(std::vector<int> list) override;
+};
+
+class CocktailSort : public SortingStrategy {
+    void sort(std::vector<int> list) override;
+};
+
+class SortingContext {
+
+private:
+    SortingStrategy *strategy;
+
+public:
+    void setSortingMethod(SortingStrategy *strategy) {
+        this->strategy = strategy;
+    }
+
+    SortingStrategy* getStrategy() {
+        return strategy;
+    }
+
+    public void sortNumbers(std::vector<int> numbers){
+        strategy->sort(numbers);
+    }
+}
+
+
 
 #endif // SORTINGALGORITHMS_H
